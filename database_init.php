5f5baa6eeb7861d4f05721337be6445d101dfb01
turbovladimir <sk8.killer@mail.ru>
2021-03-14 02:15:06 +0300

    modified:   .gitignore 	modified:   database_init.php

--- database_init.php
+++ database_init.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'bootstrap.php';
 $dsn = "mysql:dbname={$mysql_credentials['database']};host={$mysql_credentials['host']}";
-$c = new PDO($dsn, $mysql_credentials['user'], $mysql_credentials['password']);
-$c->exec('create database if not exists telebot CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;');
-$c->exec(file_get_contents(__DIR__ . '/../migrations/structure.sql'));
+
+try {
+    $c = new PDO($dsn, $mysql_credentials['user'], $mysql_credentials['password']);
+    $c->exec(file_get_contents('/migrations/structure.sql'));
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
+}
